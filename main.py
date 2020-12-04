7beae3acb58a47c192b0ba467023a3d85fb4c853
kroker42 <git@frisch.se>
2020-12-04 11:41:28 +0000

    refactored

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def read_ints(fname):
 def read_lines(fname):
     return [line for line in open(fname)]
 
+# read multiple-line records from file separated by empty line
+def read_records(fname):
+    return [record for record in open(fname).read().split('\n\n')]
+
 # Day 1
 
 # get map of pair sum -> pair for all permutation pairs in a list
@@ -124,8 +128,6 @@ class Day3Test(unittest.TestCase):
         self.assertEqual(4, trajectory(self.tree_map, 7))
         self.assertEqual(2, trajectory(self.tree_map[::2], 1))
 
-
-
 def day3():
     slope = [row[:-1] for row in read_lines('day3input.txt')]
 
@@ -136,44 +138,25 @@ def day3():
     return time.time() - start_time, task1, task2
 
 # Day 4
-def parse_passports(data):
-    passports = []
-    pp = ""
-
-    for line in data:
-        if not line:
-            passports.append(pp)
-            pp = line
-        else:
-            pp += line + " "
-    passports.append(pp)
-    return passports
-
 def parse_dict(str):
-    result = {k: v for k, v in [kv.split(':') for kv in str.split()]}
-    return result
+    return {k: v for k, v in [kv.split(':') for kv in str.split()]}
 
 # is real passport or is North Pole Credentials - with cid field missing
 def is_valid_passport(pp):
     return len(pp) == 8 or (len(pp) == 7 and "cid" not in pp.keys())
 
+# height is e.g. "156cm" or "62in", and in the valid range for each height unit
 def is_valid_height(str):
-    height = None
-    if str[-2:] == "cm" or str[-2:] == "in":
+    restrictions = {"cm": range(150, 194), "in": range(59, 77)}
+    if str[-2:] in restrictions.keys():
         height = int(re.search(r'\d+', str).group())
-    if height:
-        if str[-2:] == "cm":
-            return height in range(150, 194)
-        else:
-            return height in range(59, 77)
+        return height and height in restrictions[str[-2:]]
 
-# a # followed by exactly six characters 0-9 or a-f.
+    return False
+
+# a '#' followed by exactly six characters 0-9 or a-f.
 def is_valid_hair(str):
-    if str[0] == '#':
-        m = re.search(r"[0-9a-f]{6}", str)
-        return m and len(m.group()) == 6
-    else:
-        return False
+    return re.search(r"#[0-9a-f]{6}", str) != None
 
 # for a valid passport - are the fields also valid?
 # pre-condition; is_valid_passport(pp)
@@ -188,43 +171,25 @@ def has_valid_fields(pp):
         len(pp['pid']) == 9 and pp['pid'].isdigit()
 
 class Day4Test(unittest.TestCase):
-    data = ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
-                 "byr:1937 iyr:2017 cid:147 hgt:183cm",
-                 "",
-                 "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
-                 "hcl:#cfa07d byr:1929",
-                 "",
-                 "hcl:#ae17e1 iyr:2013",
-                 "eyr:2024",
-                 "ecl:brn pid:760753108 byr:1931",
-                 "hgt:179cm",
-                 "",
-                 "hcl:#cfa07d eyr:2025 pid:166559648",
-                 "iyr:2011 ecl:brn hgt:59in"]
-
-    pps = [parse_dict(kv) for kv in parse_passports(data)]
-
-    def test_parse_dict(self):
-        self.assertEqual({"hcl": "#cfa07d", "byr": "1929"}, parse_dict(self.data[4]))
-
-    def test_parse_passports(self):
-        self.assertEqual(["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm ",
-                          "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929 "],
-                         parse_passports(self.data[:5]))
-        self.assertEqual(4, len(self.pps))
+    data = ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
+            "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
+            "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"]
 
     def test_is_valid_passport(self):
-        self.assertTrue(is_valid_passport(self.pps[0]))
-        self.assertFalse(is_valid_passport(self.pps[1]))
-        self.assertTrue(is_valid_passport(self.pps[2]))
-        self.assertFalse(is_valid_passport(self.pps[3]))
+        exp_results = [True, False, True, False]
+        pps = [parse_dict(pp) for pp in self.data]
+        self.assertEqual(len(exp_results), len(pps))
+
+        for i in range(0, len(pps)):
+            self.assertEqual(exp_results[i], is_valid_passport(pps[i]))
 
     def test_is_valid_height(self):
+        self.assertTrue(is_valid_height("170cm"))
+        self.assertTrue(is_valid_height("60in"))
         self.assertFalse(is_valid_height("123cm"))
         self.assertFalse(is_valid_height("123in"))
         self.assertFalse(is_valid_height("123"))
-        self.assertTrue(is_valid_height("170cm"))
-        self.assertTrue(is_valid_height("60in"))
 
     def test_is_valid_hair(self):
         self.assertTrue(is_valid_hair("#12de4f"))
@@ -233,9 +198,9 @@ class Day4Test(unittest.TestCase):
         self.assertFalse(is_valid_hair("#e4f"))
 
     invalid_pps = ["eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
-                 "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
-                 "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
-                 "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"]
+                   "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
+                   "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+                   "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"]
 
     valid_pps = ["pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f",
                  "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
@@ -243,23 +208,17 @@ class Day4Test(unittest.TestCase):
                  "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"]
 
     def test_has_valid_fields(self):
-        for pp in self.invalid_pps:
-            self.assertFalse(has_valid_fields(parse_dict(pp)))
-
-        for pp in self.valid_pps:
-            self.assertTrue(has_valid_fields(parse_dict(pp)))
+        self.assertEqual(len(self.valid_pps), sum([has_valid_fields(pp) for pp in [parse_dict(d) for d in self.valid_pps]]))
+        self.assertEqual(0, sum([has_valid_fields(pp) for pp in [parse_dict(d) for d in self.invalid_pps]]))
 
 def day4():
-    data = [row[:-1] for row in read_lines('day4input.txt')]
+    data = [parse_dict(record) for record in read_records('day4input.txt')]
 
     start_time = time.time()
-    passports = [parse_dict(kv) for kv in parse_passports(data)]
-    valid_passports = [pp for pp in passports if is_valid_passport(pp)]
+
+    valid_passports = [pp for pp in data if is_valid_passport(pp)]
     task1 = len(valid_passports)
-    task2 = 0
-    for pp in valid_passports:
-        if has_valid_fields(pp):
-            task2 += 1
+    task2 = sum([has_valid_fields(pp) for pp in valid_passports])
 
     return time.time() - start_time, task1, task2
 
